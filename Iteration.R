a18e0e58202fbd6c30d4af522787623b53dec2a8
Chris <cdimapasok123@gmail.com>
2021-08-31 17:22:39 -0700

    Script to iterate through dataframe and extract columns to be used in gprofiler

---
+++ Iteration.R
@@ -0,0 +1,19 @@
+#Iterate through data frame and take each column and convert to Vector 
+#iterate through columns and maybe see if there are GO terms that are in multiple clusters?
+#iterate using other tissues????
+
+
+df <- read.csv("kendall-liver-gene-clusters-high-variance.csv")
+df
+
+
+#Create empty vector that will contain gene cluster columns
+column_vector <- vector()
+
+#for loop to iterate through df and extract columns and convert to vector 
+
+for (i in 1:ncol(df)) {
+  vector <- subset(df, select = c[1]
+
+  column_vector[i] <-                    
+}
