0942f3e8fea3d7cdf342b45c880af817c7a5eb8f
Chris <cdimapasok123@gmail.com>
2021-09-03 17:44:14 -0700

    Added new iteration function

--- Iteration.R
+++ Iteration.R
@@ -1,20 +1,27 @@
 #Iterate through data frame and take each column and convert to Vector 
 #iterate through columns and maybe see if there are GO terms that are in multiple clusters?
 #iterate using other tissues????
+library(gprofiler2)
 
-
+#load in data
 df <- read.csv("kendall-liver-gene-clusters-high-variance.csv")
 df
 
 View(df)
 
-#Create empty vector that will contain gene cluster columns
-column_vector <- vector()
 
 #for loop to iterate through df and extract columns and convert to vector 
 
-for (i in 1:ncol(df)) {
-  vector <- c(column_vector, df)
-  print(vector)
-  }
+for(i in 1:length(df)) {
+  column <- c(df, i)
+}
+
+
+
+#Now have list of vectors that can be inserted into Gprofiler
+cluster_A.2 <- gconvert(query = c(column$X), organism = "hsapiens", 
+                        target="ENSG", mthreshold = Inf, filter_na = TRUE)
+cluster_A.2
+
+
 
