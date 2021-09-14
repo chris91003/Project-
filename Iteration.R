957eb5f9bf8cc255b19c81479e5927aaa38850d0
Chris <cdimapasok123@gmail.com>
2021-09-13 23:16:03 -0700

    Added plots for all cluster types

--- Iteration.R
+++ Iteration.R
@@ -272,7 +272,7 @@ library(HPAStainR)
 
 
 
-HPA_data <- HPA_data_downloader(tissue_type = "both", save_file = TRUE)
+HPA_data <- HPA_data_downloader(tissue_type = "both", save_file = FALSE)
 HPA_data
 
 
@@ -283,7 +283,7 @@ listA.2 <- df$A.2
 listA.2
 
 stain <- HPAStainR::HPAStainR(listA.1, hpa_dat = HPA_data$hpa_dat)
-stain
+View(stain)
 
 stain_df <- as.data.frame(stain)
 stain_df
@@ -291,18 +291,31 @@ View(stain_df)
 
 newstain_df <- stain_df[-c(122:184),]
 View(newstain_df)
-#Remove Rows
+
+
+
+
+
+
+
+
+
+
+
+#A.1 Plot
 
 A.1plot <- ggplot(newstain_df, aes(x= cell_type, y= percent_high_expression)) +
   geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1)) + xlab("A.1 Cluster")
 
 A.1plot
 
+#Remove Rows
 finalstain_df <- stain_df[-c(41:184),]
 finalstain_df
 
-A.1plot1 <- ggplot(finalstain_df, aes(x= cell_type, y= percent_high_expression)) +
-  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("A.1 Cluster High Expression")
+A.1plot1 <- ggplot(finalstain_df, aes(x= cell_type, y= percent_high_expression, fill= cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("A.1 Cluster High Expression") 
+  
 
 A.1plot1
 
@@ -318,8 +331,8 @@ head(stain2_df)
 
 library(data.table)
 
-table <- stain2_df[stain2_df$cell_type %like% "LIVER",]
-View(table)
+A.2table <- stain2_df[stain2_df$cell_type %like% "LIVER",]
+View(A.2table)
 
 #Subset Rows
 
@@ -328,7 +341,300 @@ View(newstain2)
 
 #GGplot to quantify A.2
 View(newstain2)
-A.2plot2 <- ggplot(newstain2,aes(x= cell_type, y= percent_high_expression)) +
+A.2plot2 <- ggplot(newstain2,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
   geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("A.2 Cluster High Expression")
 
-A.2plot2
+
+  A.2plot2  
+
+
+  
+
+
+  
+  
+  
+  
+#B stain
+Bstain <- HPAStainR::HPAStainR(df$B, hpa_dat = HPA_data$hpa_dat)
+View(Bstain)  
+  
+B_stain <- as.data.frame(Bstain)
+
+Bmod <- B_stain[-c(11:184),]
+Bmod  
+  
+  
+Bplot <- ggplot(Bmod,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("B Cluster High Expression")  
+  
+  
+Bplot  
+
+#Bplot Liver expression
+Btable <- B_stain[B_stain$cell_type %like% "LIVER",]
+View(Btable)  
+  
+
+
+
+
+#C stain
+
+Cstain <- HPAStainR::HPAStainR(df$C, hpa_dat = HPA_data$hpa_dat)
+View(Cstain)  
+
+C_stain <- as.data.frame(Cstain)
+
+Cmod <- C_stain[-c(16:184),]
+Cmod  
+
+
+Cplot <- ggplot(Cmod,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("C Cluster High Expression")  
+
+
+Cplot  
+
+#Cplot Liver expression
+Ctable <- C_stain[C_stain$cell_type %like% "LIVER",]
+View(Ctable)  
+
+
+
+
+#D stain
+
+Dstain <- HPAStainR::HPAStainR(df$D, hpa_dat = HPA_data$hpa_dat)
+View(Dstain)  
+
+D_stain <- Dstain[-c(75:184),]
+
+Dplot <- ggplot(D_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("D Cluster High Expression")  
+
+
+Dplot  
+
+#Dplot Liver expression
+Dtable <-D_stain[D_stain$cell_type %like% "LIVER",]
+View(Dtable)  
+
+
+
+#E stain
+
+Estain <- HPAStainR::HPAStainR(df$E, hpa_dat = HPA_data$hpa_dat)
+View(Estain)  
+
+E_stain <- Estain[-c(11:184),]
+
+Eplot <- ggplot(E_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("E Cluster High Expression")  
+
+Eplot
+
+
+#F stain
+
+
+Fstain <- HPAStainR::HPAStainR(df$F, hpa_dat = HPA_data$hpa_dat)
+View(Fstain)  
+
+F_stain <- Fstain[-c(41:184),]
+
+Fplot <- ggplot(F_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("F Cluster High Expression")  
+
+
+
+Fplot
+
+
+
+#G stain
+
+Gstain <- HPAStainR::HPAStainR(df$G, hpa_dat = HPA_data$hpa_dat)
+View(Gstain)  
+
+G_stain <- Gstain[-c(26:184),]
+
+Gplot <- ggplot(G_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("G Cluster High Expression")  
+
+Gplot
+
+
+#G plot for low expression 
+
+Glow <- ggplot(G_stain,aes(x= cell_type, y= percent_low_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("G Cluster Low Expression")  
+
+
+Glow
+
+
+
+#H stain
+
+Hstain <- HPAStainR::HPAStainR(df$H, hpa_dat = HPA_data$hpa_dat)
+View(Hstain)  
+
+H_stain <- Hstain[-c(21:184),]
+
+Hplot <- ggplot(H_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("H Cluster High Expression")  
+
+Hplot
+
+
+#I.1 stain
+
+I.1stain <- HPAStainR::HPAStainR(df$I.1, hpa_dat = HPA_data$hpa_dat)
+View(I.1stain)  
+
+I.1_stain <- I.1stain[-c(21:184),]
+
+I.1plot <- ggplot(I.1_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("I.1 Cluster High Expression")  
+
+I.1plot
+
+
+#I.2 stain
+
+
+I.2stain <- HPAStainR::HPAStainR(df$I.2, hpa_dat = HPA_data$hpa_dat)
+View(I.2stain)  
+
+I.2_stain <- I.2stain[-c(31:184),]
+
+I.2plot <- ggplot(I.2_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("I.2 Cluster High Expression")  
+
+I.2plot
+
+
+#I.2 plot low
+
+I.2low_stain <- I.2stain[-c(55:184),]
+
+I.2low <- ggplot(I.2low_stain,aes(x= cell_type, y= percent_low_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("I.2 Cluster Low Expression")  
+
+
+I.2low
+
+
+
+
+
+
+
+
+
+
+
+#Attach all graphs
+
+
+par(mfrow=c(5,6))
+a1 <- ggplot(finalstain_df, aes(x= cell_type, y= percent_high_expression, fill= cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("A.1 Cluster High Expression") 
+
+newa1 <- a1 +theme(legend.position = "none")
+
+newa1
+a2 <- ggplot(newstain2,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("A.2 Cluster High Expression")
+
+newa2 <- a2 +theme(legend.position = "none")
+
+b<- ggplot(Bmod,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("B Cluster High Expression")  
+
+newb <- b +theme(legend.position = "none")
+
+c <- ggplot(Cmod,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("C Cluster High Expression")  
+
+newc <- c +theme(legend.position = "none")
+
+d<- ggplot(D_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("D Cluster High Expression")  
+
+newd <- d +theme(legend.position = "none")
+
+e<- ggplot(E_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("E Cluster High Expression")  
+
+newe <- e +theme(legend.position = "none")
+
+f<- ggplot(F_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("F Cluster High Expression")  
+
+newf <- f +theme(legend.position = "none")
+
+g<- ggplot(G_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("G Cluster High Expression")  
+
+newg <- g +theme(legend.position = "none")
+
+h<-  ggplot(H_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("H Cluster High Expression")  
+
+newh <- h +theme(legend.position = "none")
+
+i.1 <- ggplot(I.1_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("I.1 Cluster High Expression")  
+
+newi.1 <- i.1 +theme(legend.position = "none")
+
+
+i.2 <-ggplot(I.2_stain,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("I.2 Cluster High Expression")  
+
+newi.2 <- i.2 +theme(legend.position = "none")
+
+
+
+
+d1 <- Dstain[-c(61:184),]
+d2<- ggplot(d1,aes(x= cell_type, y= percent_high_expression, fill=cell_type)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust =1))+ xlab("D Cluster High Expression")  
+
+d2
+newd2 <- d2 +theme(legend.position = "none")
+
+
+install.packages("gridExtra")
+library(gridExtra)
+library(ggplot2)
+grid <- grid.arrange(newa1, newa2, newb, newc, newd, newe, newf , newg,
+             newh, newi.1, newi.2, nrow= 5, ncol= 6, widths= c(0.1, 0.1, 0.1, 0.1, 0.1, 0.1))
+
+grid1 <- grid.arrange(newa1, newa2, newb, newc, newd, newe, widths= c(1, 1, 1))
+
+grid2 <- grid.arrange(newf, newg, newh, newi.2, nrow= 1, ncol= 4, widths= c(1,1,1,1))
+View(grid)
+
+
+newgrid <- grid.arrange(newa1, newa2, newb, newc, newd2, newe, widths= c(0.6, 0.6, 0.6))
+
+
+
+
+
+#Loop to perform stain for every cluster    
+for i in 1:length(split1)) {
+  genstain <- HPAStainR::HPAStainR(split1[[i]], hpa_dat = HPA_data$hpa_dat)
+  print(genstain)
+}  
+
+View(genstain)
+
+
+#Maybe lapply
+lapply(df, function(x, HPAStainR::HPAStainR(split1[[i]], hpa_dat = HPA_data$hpa_dat) )
+
+  
